bd6337661561f3d147e213b1e7d4b3f5ca6df19c
Heyday7 <kth3447@gmail.com>
2021-03-22 22:34:52 +0900

    csrf issue를 피하기 위한 코드 작성

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ import reportWebVitals from './reportWebVitals';
 import rootSaga from './sagas'
 import { Provider } from 'react-redux'
 import { configureStore } from './store'
+import axios from 'axios'
+
+
+axios.defaults.baseURL='http://127.0.0.1:8000'
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 
 
 const store = configureStore();
